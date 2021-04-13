4ddc234a63eb558b886a07e9fcf98d26886f0d98
thuannef <liongentle@gmail.com>
2021-04-13 17:33:35 +0700

    Viet chuc nang chon linh kien trong phieu xuat

---
+++ Pizzaria1/Models/CXuLyPhieuXuat.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfLKMT.Models
+{
+    class CXuLyPhieuXuat
+    {
+        private static HttpClient hc = new HttpClient();
+        public static List<CPhieuXuat> getDSPhieuXuat()
+        {
+            try
+            {
+                string url = @"http://localhost:64275/api/phieuxuat";
+                var kq = hc.GetAsync(url);
+                kq.Wait();
+                if (kq.Result.IsSuccessStatusCode == false) return null;
+                var list = kq.Result.Content.ReadAsAsync<List<CPhieuXuat>>();
+                list.Wait();
+                return list.Result;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public static CPhieuXuat getPhieuXuat(string maPX)
+        {
+            try
+            {
+                string url = @"http://localhost:53137/api/hoadon/" + maPX;
+                var kq = hc.GetAsync(url);
+                kq.Wait();
+                if (kq.Result.IsSuccessStatusCode == false) return null;
+                var list = kq.Result.Content.ReadAsAsync<CPhieuXuat>();
+                list.Wait();
+                return list.Result;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public static IEnumerable<object> getDSChiTietPhieuXuat(CPhieuXuat px)
+        {
+            return px.ChiTietPhieuXuats.Select(x => new
+            {
+                DonGia = x.DonGia,
+                MaLK = x.MaLK,
+                TenLK = x.LinhKien.TenLK,
+                MaLoai = x.LinhKien.MaLoai,
+                SoLuong = x.SoLuong,
+                thanhtien = x.SoLuong.Value * x.DonGia.Value
+            }).ToList();
+        }
+        public static double getThanhTienPhieuXuat(CPhieuXuat px)
+        {
+            return px.ChiTietPhieuXuats.Sum(x => x.SoLuong.Value * x.DonGia.Value);
+        }
+        public static bool themPhieuXuat(CPhieuXuat px)
+        {
+            try
+            {
+                string url = @"http://localhost:53137/api/hoadon/";
+                var kq = hc.PostAsJsonAsync(url, px);
+                kq.Wait();
+                return kq.Result.IsSuccessStatusCode;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
--- Pizzaria1/qlPhieuXuat.xaml.cs
+++ Pizzaria1/qlPhieuXuat.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using wpfLKMT.Models;
 
 namespace wpfLKMT
 {
@@ -20,9 +21,95 @@ namespace wpfLKMT
     /// </summary>
     public partial class qlPhieuXuat : UserControl
     {
+        private CPhieuXuat pxTemp = new CPhieuXuat
+        {
+            MaPX = 0,
+            NgayXuat = DateTime.Now,
+            MaKH = "",
+            MaNV = "",
+            status = true,
+            TongTien = 0,
+            ChiTietPhieuXuats = new List<CChiTietPhieuXuat>()
+        };
         public qlPhieuXuat()
         {
             InitializeComponent();
+            hienthi();
+            List<CLinhKien> ds = CXuLiLinhKien.getDanhSachLinhKien();
+            if (ds == null) MessageBox.Show("Lỗi kết nối Server!!");
+            else cmbMahang.ItemsSource = ds;
+        }
+        private void hienthi()
+        {
+            List<CPhieuXuat> dsPhieuXuat = CXuLyPhieuXuat.getDSPhieuXuat();      
+            dgDSPhieuXuat.ItemsSource = dsPhieuXuat;
+        }
+
+        private void BtnChon_Click(object sender, RoutedEventArgs e)
+        {
+            int n;
+            bool isNumeric = int.TryParse(txtSoluong.Text, out n);
+            var item = cmbMahang.SelectedItem;
+            if (item == null)
+                MessageBox.Show("Vui lòng chọn linh kiện!");
+            else if (txtSoluong.Text == "")
+                MessageBox.Show("Vui lòng nhập số lượng!");
+            else if (isNumeric == false)
+            {
+                MessageBox.Show("Số lượng phải là số!");
+            }
+            else
+            {
+                CChiTietPhieuXuat ct = new CChiTietPhieuXuat();
+                CLinhKien lk = cmbMahang.SelectedItem as CLinhKien;
+                if (lk == null) return;
+                else
+                {
+                    bool flag = false;
+                    foreach (CChiTietPhieuXuat t in pxTemp.ChiTietPhieuXuats)
+                    {
+                        if (t.MaLK == lk.MaLK)
+                        {
+                            flag = true;
+                            int s = int.Parse(txtSoluong.Text) + (int)t.SoLuong;
+                            if (s == 0)
+                            {
+                                pxTemp.ChiTietPhieuXuats.Remove(t);
+                                dgChitiet.ItemsSource = CXuLyPhieuXuat.getDSChiTietPhieuXuat(pxTemp);
+                                txtThanhtien.Text = CXuLyPhieuXuat.getThanhTienPhieuXuat(pxTemp).ToString();
+                                break;
+                            }
+                            else if (s < 0)
+                            {
+                                MessageBox.Show("Số lượng không hợp lệ!!");
+                                break;
+                            }
+                            else
+                            {
+                                t.SoLuong = s;
+                                dgChitiet.ItemsSource = CXuLyPhieuXuat.getDSChiTietPhieuXuat(pxTemp);
+                                txtThanhtien.Text = CXuLyPhieuXuat.getThanhTienPhieuXuat(pxTemp).ToString();
+                                break;
+                            }
+                        }
+                    }
+                    if (flag == false)
+                    {
+                        if (int.Parse(txtSoluong.Text) <= 0)
+                            MessageBox.Show("Số lượng không hợp lệ!!");
+                        else
+                        {
+                            ct.LinhKien = lk;
+                            ct.MaLK = lk.MaLK;
+                            ct.SoLuong = int.Parse(txtSoluong.Text);
+                            ct.DonGia = lk.GiaBan;
+                            pxTemp.ChiTietPhieuXuats.Add(ct);
+                            dgChitiet.ItemsSource = CXuLyPhieuXuat.getDSChiTietPhieuXuat(pxTemp);
+                            txtThanhtien.Text = CXuLyPhieuXuat.getThanhTienPhieuXuat(pxTemp).ToString();
+                        }
+                    }
+                }
+            }
         }
     }
 }
